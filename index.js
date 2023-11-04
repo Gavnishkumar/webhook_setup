6beb21e282705107220ae5cd20660f778bd6a0d6
gavnish kumar <107535592+Gavnishkumar@users.noreply.github.com>
2023-11-04 15:57:27 +0530

    errors

--- index.js
+++ index.js
@@ -29,7 +29,10 @@ app.post('/webhook',(req,res)=>{
     const verify_token=process.env.TOKEN;
     console.log(JSON.stringify(body_param,null,2));
     if(body_param.object){
-        if(body_param.entry && body_param.entry[0].changes && body_param.entry[0].changes[0].value.messages && body_param.entry[0].changes[0].value.messages[0]){
+        if(body_param.entry &&
+             body_param.entry[0].changes && 
+             body_param.entry[0].changes[0].value.messages && 
+             body_param.entry[0].changes[0].value.messages[0]){
             let phone_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let from = body_param.entry[0].changes[0].value.messages[0].from;
             let msg_body=body_param.entry[0].changes[0].value.messages[0].text.body;
@@ -53,7 +56,7 @@ app.post('/webhook',(req,res)=>{
             //   });
             axios({
                 method:"POST",
-                url:"https://graph.facebook.com/v17.0/"+phone_no_id+"/message?access_token="+verify_token,
+                url:"https://graph.facebook.com/v17.0/"+phone_no_id+"/messages?access_token="+verify_token,
                 data:{
                     messaging_product:"whatsapp",
                     to:from,

328463cfd2b03f63a994a8812d35946720518118
gavnish kumar <107535592+Gavnishkumar@users.noreply.github.com>
2023-11-04 14:45:11 +0530

    changes version of url

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.get("/webhook",(req,res)=>{
 });
 app.post('/webhook',(req,res)=>{
     let body_param=req.body;
+   
     const verify_token=process.env.TOKEN;
     console.log(JSON.stringify(body_param,null,2));
     if(body_param.object){
@@ -33,10 +34,10 @@ app.post('/webhook',(req,res)=>{
             let from = body_param.entry[0].value.messages[0].from;
             let msg_body=body_param.entry[0].changes[0].value.messages[0].text.body;
             console.log(msg_body);
-            console.log("https://graph.facebook.com/v13.0/"+phone_no_id+"/message?access_token"+verify_token)
+            console.log("https://graph.facebook.com/v17.0/"+phone_no_id+"/message?access_token"+verify_token)
             axios({
                 method:"POST",
-                url:"https://graph.facebook.com/v13.0/"+phone_no_id+"/message?access_token"+verify_token,
+                url:"https://graph.facebook.com/v17.0/"+phone_no_id+"/message?access_token"+verify_token,
                 
                 data:{
                     messaging_product:"whatsapp",
